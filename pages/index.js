618b5165be73401a46aaa30631b98104f2c4378d
Liam Griffin-Jowett <gryffskin@gmail.com>
2023-01-06 15:24:06 +0000

    moviinn site

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">a non prod or staging branch</a>
+          Welcome to <a href="https://nextjs.org">Moviinn wheeey</a>
         </h1>
 
         <p className={styles.description}>
