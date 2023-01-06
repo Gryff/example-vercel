72aa066cb73068e5042891116fec0d6f812209f6
Liam Griffin-Jowett <gryffskin@gmail.com>
2023-01-06 16:00:09 +0000

    only build on correct branch

--- should-build.sh
+++ should-build.sh
@@ -2,12 +2,12 @@
 
 set -eux
 
-if [[ "$PROJECT_TYPE" == "production" ]] && [[ "$CURRENT_PROJECT" == "production" ]] ; then
+if [[ "$PROJECT_TYPE" == "production" ]] && [[ "$CURRENT_PROJECT" == "production" ]] && [[ "$VERCEL_GIT_COMMIT_REF" == "production" ]] ; then
   echo "✅ - PRODUCTION Build can proceed"
   exit 1;
 fi
 
-if [[ "$PROJECT_TYPE" == "staging" ]] && [[ "$URRENT_PROJECT" == "staging" ]] ; then
+if [[ "$PROJECT_TYPE" == "staging" ]] && [[ "$URRENT_PROJECT" == "staging" ]] && [[ "$VERCEL_GIT_COMMIT_REF" == "staging" ]] ; then
   echo "✅ - STAGING BUILD can proceed"
   exit 1;
 fi
