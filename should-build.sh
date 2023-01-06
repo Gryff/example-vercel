503a99e38e0d788bd2d53dd7fc0dc38c082deb9c
Liam Griffin-Jowett <gryffskin@gmail.com>
2023-01-06 15:56:23 +0000

    add bash script to determine whether should build

---
+++ should-build.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [[ "$PROJECT_TYPE" == "production" ]] && [[ "$CURRENT_PROJECT" == "production" ]] ; then
+  echo "✅ - PRODUCTION Build can proceed"
+  exit 1;
+fi
+
+if [[ "$PROJECT_TYPE" == "staging" ]] && [[ "$URRENT_PROJECT" == "staging" ]] ; then
+  echo "✅ - STAGING BUILD can proceed"
+  exit 1;
+fi
