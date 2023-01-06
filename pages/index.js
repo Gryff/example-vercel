966c639e3df0d319bec26e6ca4c2604b39856ac7
Liam Griffin-Jowett <gryffskin@gmail.com>
2023-01-06 15:10:25 +0000

    this is a non prod staging branch

--- pages/index.js
+++ pages/index.js
@@ -13,7 +13,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
+          Welcome to <a href="https://nextjs.org">a non prod or staging branch</a>
         </h1>
 
         <p className={styles.description}>
